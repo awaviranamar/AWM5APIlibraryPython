1685eab9ce153a9572a55f5dcda85dfa7341fd91
awaviranamar <aviran.amar@assetworks.com>
2025-07-08 15:01:50 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config/config.py
-
-username = "CSI"
-password = "staff"
-site = "m5dpy25ora"
-token_url = "http://m5dpy25.vgnet.volgrp.com/edge/api/token"
-api_url= "http://localhost:63324/api/v1/"
